07abbd2092e98b787b2bf1707a37ba4aa2b966dd
shiguma127 <39233270+shiguma127@users.noreply.github.com>
2018-05-13 21:32:53 +0900

    コミット

---
+++ src/main/java/PickGUI/Main.java
@@ -0,0 +1,32 @@
+package PickGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main (String[] args){
+        new Main();
+        JFrame mainFrame = new JFrame("Picker");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(320, 160);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+        Container contentPane = mainFrame.getContentPane();
+        JButton buttonE = new JButton("PickGUI.Erangel:島");
+        JButton buttonM = new JButton("PickGUI.Miramar:砂漠");
+        contentPane.add(buttonE,BorderLayout.EAST);
+        contentPane.add(buttonM,BorderLayout.WEST);
+
+        buttonE.addActionListener(e -> {
+            Erangel randame = Erangel.getRandom();
+            JOptionPane.showMessageDialog(null, randame.getname()+" が選ばれました");
+        });
+
+        buttonM.addActionListener(e -> {
+            Miramar randam = Miramar.getRundom();
+            JOptionPane.showMessageDialog(null, randam.getname()+" が選ばれました");
+        });
+
+
+    }
+}
